608a6b63bf182a65875451dc749e00da5f8d6a72
Nail Haikal <christiansensen00@gmail.com>
2022-11-04 15:25:10 +0700

    notif page update

--- user-frontend/src/views/creators/Notification.vue
+++ user-frontend/src/views/creators/Notification.vue
@@ -1,31 +1,84 @@
 <template>
-  <div v-for="item in notifications" :key="item.id">
-    <div class="flex py-2 w-full">
-      <div class="m-auto">
-        <div
-          class="bg-white w-80 mx-3 lg:w-96 rounded-lg border-gray-300 border p-3 shadow-lg"
-        >
-          <div class="flex flex-row">
-            <div class="px-2">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 1792 1792"
-                fill="#44C997"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1299 813l-422 422q-19 19-45 19t-45-19l-294-294q-19-19-19-45t19-45l102-102q19-19 45-19t45 19l147 147 275-275q19-19 45-19t45 19l102 102q19 19 19 45t-19 45zm141 83q0-148-73-273t-198-198-273-73-273 73-198 198-73 273 73 273 198 198 273 73 273-73 198-198 73-273zm224 0q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"
-                />
-              </svg>
-            </div>
-            <div class="px-10 lg:px-16">
-              <span class="font-semibold">{{ item.title }}</span>
-              <span class="block text-gray-500">{{ item.description }}</span>
+  <div class="flex justify-center items-start w-full py-3">
+    <div>
+      <div v-for="item in notifications" :key="item.id">
+        <div class="flex py-2 w-full">
+          <div class="m-auto">
+            <div
+              class="bg-white w-80 mx-3 lg:w-96 rounded-lg border-gray-300 border p-3 shadow-lg"
+            >
+              <div class="flex flex-row text-left">
+                <div>
+                  <span class="font-semibold">{{ item.title }}</span>
+                  <span class="block text-gray-500">{{
+                    item.description
+                  }}</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
+    </div>
+
+    <div
+      class="hidden md:block w-60 h-full bg-white pt-8"
+      id="sidenavSecExample"
+    >
+      <span class="text-lg">Search By Filter</span>
+      <ul class="relative px-1">
+        <li
+          v-for="(item, idx) in filters"
+          class="relative"
+          :key="'sidenav-notif-' + idx"
+        >
+          <a
+            class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-blue-600 hover:bg-blue-50 transition duration-300 ease-in-out cursor-pointer"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="primary"
+            data-bs-toggle="collapse"
+            :data-bs-target="'#collapseSidenavSecEx' + idx"
+            aria-expanded="false"
+            :aria-controls="'collapseSidenavSecEx' + idx"
+          >
+            <span>{{ item.title }}</span>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              class="w-3 h-3 ml-auto"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+            >
+              <path
+                fill="currentColor"
+                d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"
+              ></path>
+            </svg>
+          </a>
+          <ul
+            class="relative accordion-collapse collapse"
+            :id="'collapseSidenavSecEx' + idx"
+            :aria-labelledby="'sidenavSecEx' + idx"
+            data-bs-parent="#sidenavSecExample"
+          >
+            <li
+              v-for="(item, cidx) in choice"
+              class="relative"
+              :key="'sidenav-' + idx + '-' + cidx"
+            >
+              <button
+                class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-blue-600 hover:bg-blue-50 transition duration-300 ease-in-out"
+                data-mdb-ripple="true"
+                data-mdb-ripple-color="primary"
+              >
+                {{ item.title }}
+              </button>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -54,6 +107,54 @@ export default {
         {
           title: "testing 5",
           description: "description testing 5",
+        },
+        {
+          title: "testing 1",
+          description: "description testing 1",
+        },
+        {
+          title: "testing 2",
+          description: "description testing 2",
+        },
+        {
+          title: "testing 3",
+          description: "description testing 3",
+        },
+        {
+          title: "testing 4",
+          description: "description testing 4",
+        },
+        {
+          title: "testing 5",
+          description: "description testing 5",
+        },
+      ],
+      filters: [
+        {
+          title: "filter 1",
+          description: "description filter 1",
+        },
+        {
+          title: "filter 2",
+          description: "description filter 2",
+        },
+        {
+          title: "filter 3",
+          description: "description filter 3",
+        },
+      ],
+      choice: [
+        {
+          title: "filter 1",
+          description: "description filter 1",
+        },
+        {
+          title: "filter 2",
+          description: "description filter 2",
+        },
+        {
+          title: "filter 3",
+          description: "description filter 3",
         },
       ],
     };
--- user-frontend/src/views/shopping/Cart.vue
+++ user-frontend/src/views/shopping/Cart.vue
@@ -2,17 +2,7 @@
   <section
     class="px-6 py-12 md:px-12 bg-gray-100 text-gray-800 text-center lg:text-left"
   >
-    <CartItem
-      v-for="(item, idx) in cart"
-      :key="'cart-item-' + idx"
-      :count="item.count"
-      :description="item.description"
-      :id="item.id"
-      :name="item.name"
-      :price="item.price"
-      :type="item.type"
-      :imageUrl="item.image_url"
-    />
+    <CartItem />
   </section>
   <div></div>
 </template>
