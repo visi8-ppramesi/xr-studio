5163f39ee50bd1f6cd99c5fbcc9051520b9f04ee
Priya X. Pramesi <ppramesi@visi8.com>
2023-01-11 17:58:01 +0700

    cleanup some comments

--- user-frontend/src/views/assets/Assets.vue
+++ user-frontend/src/views/assets/Assets.vue
@@ -9,7 +9,6 @@
 import AssetsSidenav from "../../components/assets/AssetsSidenav.vue";
 import AssetsContent from "../../components/assets/AssetsContent.vue";
 import { Assets } from "../../firebase/collections/assets/";
-// import { buildQueries, paginationQuery } from "@/composables/queries";
 import { listFetcher } from "@/composables/listFetcher";
 export default {
   name: "assets",
@@ -29,51 +28,6 @@ export default {
 
     return { assets, query, getAssets, search, loadMore, content };
   },
-  // mounted() {
-  //   this.getAssets();
-  // },
-  // methods: {
-  //   search(data) {
-  //     const query = buildQueries(data.query);
-  //     console.log(query);
-  //     this.query = query;
-  //     const test = query.findIndex(
-  //       (v) =>
-  //         v.type === "where" &&
-  //         ["<", "<=", "!=", "not-in", ">", ">="].includes(v._op)
-  //     );
-  //     console.log(test);
-  //     this.getAssets(this.query, false);
-  //   },
-  //   loadMore() {
-  //     this.getAssets(this.query, true);
-  //   },
-  //   async getAssets(query = [], loadMore = false) {
-  //     let pQuery;
-  //     if (!loadMore) {
-  //       this.assets = [];
-  //       pQuery = paginationQuery(12, "name", null, query);
-  //     } else {
-  //       const lastDoc = this.assets[this.assets.length - 1].doc;
-  //       pQuery = paginationQuery(12, "name", lastDoc, query);
-  //     }
-
-  //     const [currentCount, assets] = await Promise.all([
-  //       Assets.getCount(pQuery),
-  //       Assets.getDocuments(pQuery),
-  //     ]);
-  //     if (currentCount < 12) {
-  //       this.$refs.assetsContent.disableLoadMore();
-  //     }
-  //     this.assets.push(...assets);
-  //   },
-  // },
-  // data() {
-  //   return {
-  //     assets: [],
-  //     query: [],
-  //   };
-  // },
 };
 </script>
 
--- user-frontend/src/views/equipments/Equipments.vue
+++ user-frontend/src/views/equipments/Equipments.vue
@@ -13,8 +13,6 @@
 import EquipmentsSidenav from "../../components/equipments/EquipmentsSidenav.vue";
 import EquipmentsContent from "../../components/equipments/EquipmentsContent.vue";
 import { Equipments } from "../../firebase/collections/equipments";
-// import { buildQueries, paginationQuery } from "@/composables/queries";
-// import isNil from "lodash/isNil";
 import { listFetcher } from "@/composables/listFetcher";
 export default {
   name: "equipments",
@@ -34,51 +32,6 @@ export default {
 
     return { equipments, query, getEquipments, search, loadMore, content };
   },
-  // mounted() {
-  //   this.getEquipments();
-  // },
-  // methods: {
-  //   search(data) {
-  //     const query = buildQueries(data.query);
-  //     console.log(query);
-  //     this.query = query;
-  //     const test = query.findIndex(
-  //       (v) =>
-  //         v.type === "where" &&
-  //         ["<", "<=", "!=", "not-in", ">", ">="].includes(v._op)
-  //     );
-  //     console.log(test);
-  //     this.getEquipments(this.query, false);
-  //   },
-  //   loadMore() {
-  //     this.getEquipments(this.query, true);
-  //   },
-  //   async getEquipments(query = [], loadMore = false) {
-  //     let pQuery;
-  //     if (!loadMore) {
-  //       this.equipments = [];
-  //       pQuery = paginationQuery(12, "name", null, query);
-  //     } else {
-  //       const lastDoc = this.equipments[this.equipments.length - 1].doc;
-  //       pQuery = paginationQuery(12, "name", lastDoc, query);
-  //     }
-
-  //     const [currentCount, equipments] = await Promise.all([
-  //       Equipments.getCount(pQuery),
-  //       Equipments.getDocuments(pQuery),
-  //     ]);
-  //     if (currentCount < 12) {
-  //       this.$refs.equipmentsContent.disableLoadMore();
-  //     }
-  //     this.equipments.push(...equipments);
-  //   },
-  // },
-  // data() {
-  //   return {
-  //     equipments: [],
-  //     query: [],
-  //   };
-  // },
 };
 </script>
 
